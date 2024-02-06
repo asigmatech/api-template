7234c47f8d2731cdc35a446c70f5a739eb62db3a
Arnold Kigonya <arnold.kigonya@asigmacapital.com>
2024-02-06 21:11:24 +0300

    Removed docker file

--- AsigmaApiTemplate.Api/obj/Debug/net7.0/AsigmaApiTemplate.Api.AssemblyInfo.cs
+++ AsigmaApiTemplate.Api/obj/Debug/net7.0/AsigmaApiTemplate.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AsigmaApiTemplate.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a75422a85b0b58ac35c27eac43594b938013e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AsigmaApiTemplate.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AsigmaApiTemplate.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
