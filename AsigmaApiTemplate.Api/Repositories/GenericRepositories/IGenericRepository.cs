3fe1d96d04f73fad80dff7d5170e201f83b2dd21
Arnold Kigonya <arnold.kigonya@asigmacapital.com>
2024-02-06 21:47:21 +0300

    Added gitignore

---
+++ AsigmaApiTemplate.Api/Repositories/GenericRepositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using AsigmaApiTemplate.Api.Helpers;
+using AsigmaApiTemplate.Api.Models;
+
+namespace AsigmaApiTemplate.Api.Repositories.GenericRepositories;
+
+public interface IGenericRepository<T> where T : IEntity
+{
+    Task<PaginatedList<T>> GetAllAsync();
+    
+    Task<T?> GetByIdAsync(Guid id);
+    
+    Task<T> InsertAsync(T entity);
+    
+    Task<T> UpdateAsync(T entity);
+    
+    Task DeleteAsync(Guid id);
+}
---
+++ AsigmaApiTemplate.Tests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using AsigmaApiTemplate.Api;
+using AsigmaApiTemplate.Api.Controllers;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AsigmaApiTemplate.Tests.Controllers;
+
+public class WeatherForecastControllerTests
+{
+    private readonly WeatherForecastController _weatherForecastController;
+
+    public WeatherForecastControllerTests()
+    {
+        var logger = A.Fake<ILogger<WeatherForecastController>>();
+        var httpContextAccessor = A.Fake<IHttpContextAccessor>();
+        _weatherForecastController = new WeatherForecastController(logger, httpContextAccessor);
+    }
+
+    [Fact]
+    public void WeatherForecastController_GetWeatherV1_ReturnsFiveWeatherObjects()
+    {
+        //Arrange
+        
+        
+        //Act
+        var result = _weatherForecastController.GetWeatherV1();
+        var weatherForecasts = result as WeatherForecast[] ?? result.ToArray();
+
+        //Assert
+        weatherForecasts.Should().HaveCount(5);
+        weatherForecasts.Should()
+            .OnlyHaveUniqueItems();
+        weatherForecasts.Should().NotContainNulls();
+
+        weatherForecasts.ToList().ForEach(forecast =>
+        {
+            forecast.Date.Should().BeOnOrAfter(DateOnly.FromDateTime(DateTime.Now)
+            );
+            forecast.TemperatureC.Should().BeInRange(-20, 55);
+            forecast.Summary.Should().NotBeNull();
+        });
+    }
+}
